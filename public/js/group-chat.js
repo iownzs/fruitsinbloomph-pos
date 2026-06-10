fc45e3ab882ad5e75c054a653b85af44dd109fbd
iownzs <looonieverse@gmail.com>
2026-06-10 21:02:23 +0800

    Extend Android desktop chat closer to bottom

--- public/js/group-chat.js
+++ public/js/group-chat.js
@@ -2258,7 +2258,7 @@ function updateGroupChatViewportHeight(){
   const isMobileDesktop = typeof isGroupChatMobileDesktopMode === "function" && isGroupChatMobileDesktopMode();
 
   const height = isMobileDesktop
-    ? Math.min(viewportHeight, Math.round(screenHeight * 1.75))
+    ? Math.min(viewportHeight, Math.round(screenHeight * 1.90))
     : viewportHeight;
 
   document.documentElement.style.setProperty("--group-chat-vh", `${height}px`);
