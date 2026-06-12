7ed86230a6cb9410a7f7d36a6418dba465739bd1
iownzs <looonieverse@gmail.com>
2026-06-12 21:19:06 +0800

    Restore best Android desktop chat height

--- public/js/group-chat.js
+++ public/js/group-chat.js
@@ -2258,7 +2258,7 @@ function updateGroupChatViewportHeight(){
   const isMobileDesktop = typeof isGroupChatMobileDesktopMode === "function" && isGroupChatMobileDesktopMode();
 
   const height = isMobileDesktop
-    ? Math.min(viewportHeight, Math.round(screenHeight * 1.90))
+    ? Math.min(viewportHeight, Math.round(screenHeight * 1.85))
     : viewportHeight;
 
   document.documentElement.style.setProperty("--group-chat-vh", `${height}px`);
