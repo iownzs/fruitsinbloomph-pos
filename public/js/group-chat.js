5132c9ddd4c1b9a6076734ce810420c72bbe5f06
iownzs <looonieverse@gmail.com>
2026-06-19 01:47:23 +0800

    Open pinned messages as floating card

--- public/js/group-chat.js
+++ public/js/group-chat.js
@@ -2457,11 +2457,6 @@ function closeGroupChatPinnedPanel(event){
       arrow.textContent = "›";
     }
   }
-
-  const chatMain = document.querySelector(".group-chat-ref-chat");
-  if(chatMain){
-    chatMain.classList.remove("pinned-inline-open");
-  }
 }
 
 function toggleGroupChatPinnedPanel(event){
@@ -2489,11 +2484,6 @@ function toggleGroupChatPinnedPanel(event){
   }
 
   applyGroupChatViewState();
-
-  const chatMain = document.querySelector(".group-chat-ref-chat");
-  if(chatMain){
-    chatMain.classList.toggle("pinned-inline-open", Boolean(groupChatPinnedPanelOpen));
-  }
 }
 
 function toggleGroupChatOrderPreview(){
